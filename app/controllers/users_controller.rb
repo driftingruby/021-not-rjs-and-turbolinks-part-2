5d8d2efb90c4a4f4a5189840b3c260b88c08580d
kobaltz <dave@k-innovations.net>
2015-11-09 23:11:16 -0500

    020

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    # redirect_to @user, notice: 'User was successfully created.'
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to @user, notice: 'User was successfully updated.'
+  end
+
+  def destroy
+    @user_id = @user.id
+    @user.destroy
+    # redirect_to users_url, notice: 'User was successfully destroyed.'
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email_address)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  user = User.new
+  user.first_name = Faker::Name.first_name
+  user.last_name = Faker::Name.last_name
+  user.email_address = Faker::Internet.email
+  user.save!
+end
